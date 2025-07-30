fa041bd961bb1be1d47d1733e51f0226486cb94c
SolracHQ <carlos.lizalda1@gmail.com>
2025-07-30 17:37:32 -0500

    feat: add health check endpoint and update router exports

--- api/src/routers/__init__.py
+++ api/src/routers/__init__.py
@@ -2,5 +2,6 @@ from .users import router as users_router
 from .flights import router as flights_router
 from .bookings import router as bookings_router
 from .chat import router as chat_router
+from .health_check import router as health_check_router
 
-__all__ = ["users_router", "flights_router", "bookings_router", "chat_router"]
+__all__ = ["users_router", "flights_router", "bookings_router", "chat_router", "health_check_router"]
